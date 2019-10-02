cd4caeff75748c738b3334a76cb2e53091f40ff5
Z <zhipenghou2022@u.northwestern.edu>
2019-10-02 13:23:46 -0500

    returndataforplot

--- QAWebServer/testhandler.py
+++ QAWebServer/testhandler.py
@@ -23,9 +23,41 @@ class TestHandler(QABaseHandler):
         databaseid = 'mydatabase'
         collectionid = 'uploaddata'
         TS_Boosting_predict(start=start, end=end, by=by, databaseid=databaseid, collectionid=collectionid)
-        print("token")
-        test = {'token': 'success'}
-        self.write(json.dumps(test))
+
+
+        collection_prediction = database.prediction
+        ref_prediction = collection_prediction.find()
+        prediction = pd.DataFrame(list(ref_prediction)).drop(columns = '_id')
+
+        prediction_json = {
+            'yAxisData': list(prediction['predict']),
+            'xAxisData': list(map(lambda x : x.split(' ')[0],list(prediction['datetime']))),
+            'label': 'Future',
+            'colorPicked': '#519e19'
+        }
+
+
+        collection_past = database.uploaddata
+        ref_past = collection_past.find()
+        past = pd.DataFrame(list(ref_past)).drop(columns = '_id')
+
+
+        past_json = {
+            'yAxisData': list(past['y']),
+            'xAxisData': list(map(lambda x : x.split(' ')[0],list(past['datetime']))),
+            'label': 'Future',
+            'colorPicked': '#519e19'
+        }
+
+        messagebody = {
+            'past': past_json,
+            'future':prediction_json
+        }
+
+
+
+        self.write(messagebody)
+        #self.write(json.dumps(prediction_json))
 
     def options(self, *args, **kwargs):
         self.set_status(204)
